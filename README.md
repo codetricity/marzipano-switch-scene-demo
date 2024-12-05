28d3772cabeea3f53c29df49a8c0aaf3f397216a
Craig <craig@oppkey.com>
2024-12-05 11:32:58 -1000

    minimal example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minimal Marzipano Example
+
+![screenshot](readme_assets/screenshot.png)
+
+Minimal example of using open source 360 image viewer from Google.
+
+[marzipano site](https://www.marzipano.net/)
---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Create viewer.
+var viewer = new Marzipano.Viewer(document.getElementById('pano'));
+
+// Create source.
+var source = Marzipano.ImageUrlSource.fromString(
+  "https://theta-image-api.s3.amazonaws.com/theta_images/R0010217.JPG"
+);
+
+// Create geometry.
+var geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+
+// Create view.
+var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
+var view = new Marzipano.RectilinearView({ yaw: Math.PI }, limiter);
+
+// Create scene.
+var scene = viewer.createScene({
+  source: source,
+  geometry: geometry,
+  view: view,
+  pinFirstLevel: true
+});
+
+// Display scene.
+scene.switchTo();
