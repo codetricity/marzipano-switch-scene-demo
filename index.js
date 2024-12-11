9d7622451339d1a68e82aecdb3e055fb7ae493e9
Craig <craig@oppkey.com>
2024-12-10 17:17:02 -1000

    added thumbnails

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 // Create viewer.
-var viewer = new Marzipano.Viewer(document.getElementById('pano'));
+var viewer = new Marzipano.Viewer(document.getElementById("pano"));
 
 // Create geometry.
 var geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
 
 // Create view.
-var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
+var limiter = Marzipano.RectilinearView.limit.traditional(
+  1024,
+  (100 * Math.PI) / 180
+);
 var view = new Marzipano.RectilinearView({ yaw: Math.PI }, limiter);
 
 // create array of filenames
 const imageFileNames = [
-    "stanford.jpeg",
-    "carlsbad_1.jpg",
-    "carlsbad_2.jpg",
-    "room_5_5k.jpeg",
-    "palo_alto_office_11k.jpeg",
-    "honda-insight.jpeg",
-    "audi-car.jpeg"
+  "stanford.jpeg",
+  "carlsbad_1.jpg",
+  "carlsbad_2.jpg",
+  "room_5_5k.jpeg",
+  "palo_alto_office_11k.jpeg",
+  "honda-insight.jpeg",
+  "audi-car.jpeg",
 ];
 
 // Create Marzipano scenes and HTML buttons to hold each Marzipano scene
 const buttons = document.getElementById("buttons");
 
 for (let i = 0; i < imageFileNames.length; i++) {
+  // create an unique Marzipano scene for each image
+  let scene = viewer.createScene({
+    source: Marzipano.ImageUrlSource.fromString(`images/${imageFileNames[i]}`),
+    geometry: geometry,
+    view: view,
+  });
 
-    // create an unique Marzipano scene for each image
-    let scene = viewer.createScene({
-        source: Marzipano.ImageUrlSource.fromString(
-          `images/${imageFileNames[i]}`
-        ),
-        geometry: geometry,
-        view: view,
-      });
-  
-    // create HTML button and switch scene when button is clicked  
-    let buttonSwitchImage = document.createElement("button");
-    buttonSwitchImage.textContent = i + 1;
-    buttonSwitchImage.addEventListener('click', function() {
-        scene.switchTo();
-    })
-    buttons.appendChild(buttonSwitchImage);
-    // Display first scene.
-    if (i == 0) {scene.switchTo()}
+  let thumbnail = document.createElement("img");
+  thumbnail.src = `images/thumbs/${imageFileNames[i]}`; 
+  thumbnail.className = "thumb-button"
+  thumbnail.style.cursor = "pointer"; // Change cursor to pointer for interactivity
+  thumbnail.addEventListener("click", function () {
+    scene.switchTo();
+  });
+  buttons.appendChild(thumbnail);
+  // Display first scene.
+  if (i == 0) {
+    scene.switchTo();
+  }
 }
-
-
